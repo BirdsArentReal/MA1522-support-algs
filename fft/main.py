a6d2816781f8fb77b7821b29c3a4193b020c8dfb
BirdsArentReal <rsyj04@gmail.com>
2026-01-02 21:03:11 +0800

    updated main.py for better examples

--- fft/main.py
+++ fft/main.py
@@ -17,21 +17,23 @@ poly2 = [f() for _ in range(deg2)]
 ans1, _ = timeit(lambda: mult_poly(clone(poly1), clone(poly2)), 5)
 ans2, _ = timeit(lambda: naive_mult_poly(clone(poly1), clone(poly2)), 5)
 
-
-# print(FFT([-1, -1, -1, -1, 1, 1, 1, 1]))
-
-n = 2**2
+# checking well-known periodic waves to make sure it works
+n = 2**3
 square_wave = [-1] * n + [1] * n
 square_pulse = [0] * (n//2) + [1] * n + [0] * (n//2)
 
 sine_x = [0, -1, 0, 1]
 sine_2x = [0, -1, 0, 1, 0, -1, 0, 1]
+cos_8x = [1, 0, -1, 0, 1, 0, -1, 0, 1, 0, -1, 0, 1, 0, -1, 0]
 
 ans_sq_wave, _ = timeit(lambda: FFT(square_wave))
 ans_sq_pulse, _ = timeit(lambda: FFT(square_pulse))
 
 ans_sine, _ = timeit(lambda: IFFT(FFT(sine_x)))
-print(display(ans_sine))
 
 ans_sine2, _ = timeit(lambda: FFT(IFFT(sine_2x)))
+ans_cos3, _ = timeit(lambda: FFT(cos_3x))
+
+print(display(ans_sine))
 print(display(ans_sine2))
+print(display(ans_cos3))
